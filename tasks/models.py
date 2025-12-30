c528ace641cea287f857f1f66128b691998478d5
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-12-30 09:50:58 +0100

    taskmodel with admin setup

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,41 @@
+from django.utils import timezone
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ('Low', 'Low'),
+        ('Medium', 'Medium'),
+        ('High', 'High'),
+    ]
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed')
+    ]
+    title = models.CharField(max_length = 200)
+    description = models.TextField(blank=True)
+    due_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='Medium')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+    updated_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name='tasks', on_delete=models.CASCADE)
+    completed_at = models.DateTimeField(null=True, blank=True)
+    
+    def clean(self):
+        if self.due_date < timezone.now().date():
+            raise ValidationError("Due date must be in the future.")
+        
+    def mark_complete(self):
+        self.status = 'completed'
+        self.completed_at = timezone.now()
+        self.save()
+
+    def mark_incomplete(self):
+        self.status = 'pending'
+        self.completed_at = None
+        self.save()
+
+
