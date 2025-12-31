7d179b62bdaa38e6f1408de3e8ae2c638e3b374c
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2025-12-31 12:58:29 +0100

    Add users app with registration and login endpoints

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, UserViewSet
+from .views import TaskViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
-router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = router.urls
--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,6 @@
-from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 from .models import Task
-from .serializers import TaskSerializer, UserSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
-
+from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
@@ -23,20 +12,3 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
-class LoginView(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data["token"])
-        return Response({
-            "token": token.key,
-            "user_id": token.user_id
-        })
-
-class LogoutView(viewsets.ViewSet):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def create(self, request):
-        request.auth.delete()
-        return Response(status=204)
