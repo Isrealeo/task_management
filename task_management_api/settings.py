0ee6ffe27347a49a8d9b27a77bfa6f322faf9123
Nwaminogbe Isreal <nwaminogbeisreal@gmail.com>
2026-01-02 13:12:09 +0100

    prepare for deployment

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b@9ate+&_@o6zthf=kkn*w+xc5c52cnowq%51!#6kg8slb3$qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['task_management_api.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
