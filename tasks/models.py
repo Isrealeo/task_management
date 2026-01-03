055159370e4df02f6f264039ba4a4d281e6b1342
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2026-01-03 12:45:42 +0100

    Add related_name to Category owner field

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'categories')
 
     def __str__(self):
         return self.name
