ae8e3531a10c66b3f4e489b300bab4daf5ec96f9
Nwaminogbe Isreal Ohunmaehuni <nwaminogbeisreal@gmail.com>
2026-01-03 12:36:15 +0100

    Import Category model in views.py

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.decorators import action
 from django.db.models import Q
-from .models import Task
+from .models import Task, Category
 from .serializers import TaskSerializer, CategorySerializer
 from django.http import HttpResponse
 
