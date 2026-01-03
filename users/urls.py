055f3e6c97723a49a8e8d952befb95b0fcf551fa
isrealeo <nwaminogbeisreal@gmail.com>
2026-01-03 10:26:48 +0000

    add trailing slah to make endpoint consistent

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from .views import LoginView, RegisterView, UserViewSet
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("register", RegisterView.as_view(), name='register'),
+    path("register/", RegisterView.as_view(), name='register'),
     path("users/", UserViewSet.as_view({'get': 'list'}), name="user-list"),
 ]
